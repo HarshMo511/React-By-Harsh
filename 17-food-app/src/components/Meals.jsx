6ba250f0ffc9cf4e365aade1537897c72da15b97
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-06 18:26:46 +0530

    Create two component to show the menu

---
+++ 17-food-app/src/components/Meals.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        console.error("Failed to fetch meals:", response.statusText);
+        return;
+      }
+
+      const meals = await response.json();
+      if (!Array.isArray(meals)) {
+        console.error("Expected meals array but got:", meals);
+        return;
+      }
+
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
