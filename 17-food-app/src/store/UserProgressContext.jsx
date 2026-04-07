3bec95eab425bd241167b765147276e365ce6a17
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-07 14:06:40 +0530

    do button working on cart section

---
+++ 17-food-app/src/store/UserProgressContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "",
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export function UserProgressContextProvider({ children }) {
+  const [userProgress, setUserprogress] = useState();
+
+  function showCart() {
+    setUserprogress("cart");
+  }
+
+  function hideCart() {
+    setUserprogress("");
+  }
+
+  function showCheckout() {
+    setUserprogress("checkout");
+  }
+
+  function hideCheckout() {
+    setUserprogress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart,
+    hideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <UserProgressContext.Provider value={userProgressCtx}>
+      {children}
+    </UserProgressContext.Provider>
+  );
+}
+
+export default UserProgressContext;
