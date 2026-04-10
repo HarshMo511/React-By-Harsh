84fa8a75fbd6496e6cf7666b92b8a902fb8bb954
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-10 18:34:38 +0530

    Create two pages(HomePage, Products) with router

--- 22-React-router/src/App.js
+++ 22-React-router/src/App.js
@@ -1,5 +1,28 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/Products" element={<ProductsPage />} />
+//   </Route>,
+// );
+
+// const router = createBrowserRouter(routerDefinitions);
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/Products", element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
