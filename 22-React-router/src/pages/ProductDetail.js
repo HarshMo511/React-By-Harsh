1628356f4c41edf8afb2f6f00e3ab6a12f18c4e9
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-11 12:24:02 +0530

    understand how react router work

--- 22-React-router/src/pages/ProductDetail.js
+++ 22-React-router/src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function ProductDetailPage() {
   const parsms = useParams();
@@ -7,6 +7,11 @@ function ProductDetailPage() {
     <>
       <h1>Product Details!</h1>
       <p>{parsms.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 }
