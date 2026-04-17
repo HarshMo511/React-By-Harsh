77bfe5d4ce653875d6196c0e88785820071e6eb2
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-17 17:25:36 +0530

    feat: add global header with navigation links

--- 27-onwards-foodies-starting-project/app/page.js
+++ 27-onwards-foodies-starting-project/app/page.js
@@ -1,9 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <p style={{ color: "white", textAlign: "center" }}>
+        <Link href="/meals" style={{ color: "white", textAlign: "center" }}>
+          Meals
+        </Link>
+      </p>
+      <p style={{ color: "white", textAlign: "center" }}>
+        <Link
+          href="/meals/share"
+          style={{ color: "white", textAlign: "center" }}
+          >
+          Share Meals
+        </Link>
+          </p>
+      <p style={{ color: "white", textAlign: "center" }}>
+        <Link
+          href="/community"
+          style={{ color: "white", textAlign: "center" }}
+          >
+          Community
+        </Link>
+          </p>
     </main>
   );
 }
