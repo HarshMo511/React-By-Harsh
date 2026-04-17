a2a9451c44d740bda3130529ac7d4c6a11f56be4
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-17 15:17:54 +0530

    feat: add cross-page navigation links and dynamic page titles

---
+++ 26-inro-next.js/app/blog/[slug]/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function BlogPostPage({ params }) {
+  return (
+    <main>
+      <h1>Blog Post</h1>
+      <p>{params.slug}</p>
+      <p>
+        <Link href="/blog">Blog</Link>
+      </p>
+    </main>
+  );
+}
