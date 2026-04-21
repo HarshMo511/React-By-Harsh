b025d972fa93e692311c41801e6ae1c7acfcf411
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-21 11:14:06 +0530

    add selected image privew box

--- 27-onwards-foodies-starting-project/components/meals/image-picker.js
+++ 27-onwards-foodies-starting-project/components/meals/image-picker.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
+import Image from "next/image";
 import classes from "./image-picker.module.css";
 
 export function ImagePicker({ label, name }) {
@@ -11,10 +12,37 @@ export function ImagePicker({ label, name }) {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -22,6 +50,8 @@ export function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
