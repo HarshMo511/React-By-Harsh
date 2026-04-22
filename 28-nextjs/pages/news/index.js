d9910149e6413b94eee9c38a26c77fb7265a1632
harsh moradiya <harshmoradiya05@gmail.com>
2026-04-22 12:19:44 +0530

    add Link to navigate on same page

--- 28-nextjs/pages/news/index.js
+++ 28-nextjs/pages/news/index.js
@@ -1,7 +1,20 @@
 // our-domain.com/news
+import Link from "next/link";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJs Is A Great Framework
+          </Link>
+        </li>
+        <li>something Else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
